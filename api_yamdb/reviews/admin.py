ccbda5a6ffb5286554f2238d625803a1eb88d00b
обрембальский влад <vobrembalskii@gmail.com>
2022-05-31 19:43:23 +0300

    написал приложение reviews для отзывов и комментариев

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Review, Comment
 
 # Register your models here.
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'text', 'pub_date', 'score')
+    empty_value_display = '-пусто-'
+    search_fields = ('text',)
+    list_filter = ('title',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('review', 'author', 'text', 'pub_date')
+    empty_value_display = '-пусто-'
+    search_fields = ('text',)
+    list_filter = ('review',)
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Review, ReviewAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        'Title',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        help_text='введите произведения',
+        verbose_name='Произведения',
+    )
+    text = models.TextField(
+        help_text='введите текст отзыва',
+        verbose_name='текст отзыва')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        help_text='введите дату публикации ',
+        verbose_name='дата публикации')
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        help_text='введите автора',
+        verbose_name='Автор',
+    )
+    score = models.IntegerField(
+        help_text='укажите оценку произведения',
+        verbose_name='оценка',
+    )
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        'Review',
+        on_delete=models.CASCADE,
+        related_name='comments',
+        help_text='введите отзыв',
+        verbose_name='Отзыв',
+    )
+    text = models.TextField(
+        help_text='введите текст комментария',
+        verbose_name='текст комментария')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        help_text='введите дату публикации ',
+        verbose_name='дата публикации')
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        help_text='введите автора',
+        verbose_name='Автор',
+    )
